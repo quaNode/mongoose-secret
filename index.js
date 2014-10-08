541fc25170310e49d517bc2e5fae029fb3fca411
Zlatko Fedor <zfedor@gmail.com>
2014-10-09 12:51:36 +1300

    first version

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var crypto = require('crypto');
+
+function createSecret(size) {
+	var hex = crypto.randomBytes(size).toString('hex');
+	return hex.substring(0, size);
+}
+
+module.exports = function updatedPlugin (schema, options) {
+	//prepare arguments
+	options = options || {};
+	var path = options.path || 'secret';
+	var size = options.size || 32;
+
+	schema.path(path, { type: String, index: true });
+
+	schema.pre('save', function (next) {
+		if(this.get(path)) {
+			next();
+		}
+		
+		this.set(path, createSecret(size));
+		next();
+	});
+};
